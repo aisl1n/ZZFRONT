6d33b8106970ec842aa4b59d23870f3cadfaf917
aisl1n <aislanarchy@gmail.com>
2024-10-07 13:32:46 -0300

    feat: Added layout to cover the entire application based on navbar and footer.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Layout from '@/layout';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Commissioner } from 'next/font/google';
+
+const commissioner = Commissioner({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={commissioner.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
+  );
 }
