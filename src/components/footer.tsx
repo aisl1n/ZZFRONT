7f6d8ec754513f3c15b8f23b904ec4e1816412f2
aisl1n <aislanarchy@gmail.com>
2024-10-08 15:53:25 -0300

    fix: Removed unnecessary css classes and adjust to text-xs

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { FOOTER_TEXT } from "@/constants/categories";
 export default function Footer() {
   return (
     <>
-      <footer className="bottom-0 flex h-[167px] w-full items-center justify-center bg-black p-4 text-center text-[12px] text-white">
+      <footer className="flex w-full items-center justify-center bg-black p-4 text-center text-xs text-white">
         <p>{FOOTER_TEXT}</p>
       </footer>
     </>
