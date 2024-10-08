42fdb634280735350889441e6a1d0cef05e09fb6
aisl1n <aislanarchy@gmail.com>
2024-10-08 09:26:00 -0300

    fix: Removed fixed property

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <footer className="fixed bottom-0 flex h-[167px] w-full items-center justify-center bg-black p-4 text-center text-[12px] text-white">
+      <footer className="bottom-0 flex h-[167px] w-full items-center justify-center bg-black p-4 text-center text-[12px] text-white">
         <p>
           © 2024, Arezzo. Todos os direitos reservados. ZZAB Comércio de
           Calçados Ltda. | Rua África do Sul, 2280. Padre Mathias, Cariacica/ES.
