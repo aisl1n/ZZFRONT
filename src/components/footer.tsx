a1846ca42a8ca56d15a5d71d50637900f6d9bf12
aisl1n <aislanarchy@gmail.com>
2024-10-08 09:31:58 -0300

    fix: Adjust footer text to be a constant

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,10 @@
+import { FOOTER_TEXT } from "@/constants/categories";
+
 export default function Footer() {
   return (
     <>
       <footer className="bottom-0 flex h-[167px] w-full items-center justify-center bg-black p-4 text-center text-[12px] text-white">
-        <p>
-          © 2024, Arezzo. Todos os direitos reservados. ZZAB Comércio de
-          Calçados Ltda. | Rua África do Sul, 2280. Padre Mathias, Cariacica/ES.
-          CEP: 29157-900 | CNPJ: 07.900.208/0077-04
-        </p>
+        <p>{FOOTER_TEXT}</p>
       </footer>
     </>
   );
