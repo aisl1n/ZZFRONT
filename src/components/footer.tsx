778ecc2b6c3e4fbdfb28062fd751e3d6f25530c0
aisl1n <aislanarchy@gmail.com>
2024-10-08 09:20:40 -0300

    feat: Finished footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,13 @@
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <footer className="fixed bottom-0 flex h-[167px] w-full items-center justify-center bg-black p-4 text-center text-[12px] text-white">
+        <p>
+          © 2024, Arezzo. Todos os direitos reservados. ZZAB Comércio de
+          Calçados Ltda. | Rua África do Sul, 2280. Padre Mathias, Cariacica/ES.
+          CEP: 29157-900 | CNPJ: 07.900.208/0077-04
+        </p>
+      </footer>
+    </>
+  );
 }
